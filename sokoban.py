52581068c3f994d921fa7a39050dbd2d53e6f456
JesusYael <98858549+JesusYael@users.noreply.github.com>
2022-03-25 15:04:49 +0000

    Puntos 1,2,3,4

--- sokoban.py
+++ sokoban.py
@@ -8,49 +8,59 @@ class Sokoban:
   2- Muñeco
   3- Camino
   4- Metas
+  5- Muñeco_meta
+  6- Caja_meta
   """
-  posicion_columna=4 #Posicion inicial del muñeco
-  mapa= [1,3,3,3,2,3,3,3,1] #Mapa inicial del juego
+  #Mapa inicial del juego
+  mapa= [
+    [1,3,3,3,3,3,3,3,3,1],    
+    [1,3,3,3,3,3,3,3,3,1],
+    [1,3,3,3,2,3,3,3,3,1],
+    [1,3,3,3,3,3,3,3,3,1],
+    [1,3,3,3,3,3,3,3,3,1],
+    [1,3,3,3,3,3,3,3,3,1],
+  ]
+  #Posicion inicial del muñeco
+  muneco_columna=4
+  muneco_fila=2
+  
 
   def __init__(self): #Metodo para inicializar el objeto
     pass #Inicializacion del objeto
 
   def imprimirMapa(self): #Metodo para imprimir el mapa
-    for i in self.mapa: #Recorre cada posicion del mapa
-      if i==3: #Si el elemento es un 3 (camino)
-        print(" ",end="") #Imprime " " y no da salto de linea
-      elif i==2: #Si el elemento es un 2 (muñeco)
-        print(chr(64),end="") #Imprime @ y no da salto de linea
-      elif i==1: #Si el elemento es un 1 (pared)
-        print("|",end="") #Imprime | y no da salto de linea
-      else: #De lo contrario
-        print(i,end="") #Imprime el elemento encontrado
-    print() #Imprime una linea en blanco
+    for fila in self.mapa:
+      print(fila)
+      
 
   def moverDerecha(self): #Metodo para mover el personaje a la derecha
-    self.posicion_columna +=1 #Actualiza la posicion a la derecha
-    self.mapa[self.posicion_columna]=2 #Coloca al muñeco en la nueva posicion
-    self.mapa[self.posicion_columna -1]=3 #Coloca un espacio donde estaba el muñeco
-    self.imprimirMapa() #Imprime el mapa actualizado
+    if self.mapa[self.muneco_fila][self.muneco_columna]==2 and self.mapa[self.muneco_fila][self.muneco_columna+1]==3:
+      self.mapa[self.muneco_fila][self.muneco_columna]=3
+      self.mapa[self.muneco_fila][self.muneco_columna+1]=2
+      self.muneco_columna+=1
+      
+      
+  def moverIzquierda(self): 
+    if self.mapa[self.muneco_fila][self.muneco_columna]==2 and self.mapa[self.muneco_fila][self.muneco_columna-1]==3:
+      self.mapa[self.muneco_fila][self.muneco_columna]=3
+      self.mapa[self.muneco_fila][self.muneco_columna-1]=2
+      self.muneco_columna+=1
+      
 
-  def moverIzquierda(self): #Metodo para mover el personaje a la izquierda
-    self.posicion_columna -=1 #Actualiza la posicion a la izquierda
-    self.mapa[self.posicion_columna]=2 #Coloca al muñeco en la nueva posicion
-    self.mapa[self.posicion_columna +1]=3 #Coloca un espacio donde estaba el muñeco
-    self.imprimirMapa() #Imprime el mapa actualizado
-
-juego= Sokoban() #Crea un objeto del juego Sokoban
-juego.imprimirMapa() #Imprime el mapa
 
 instrucciones= "q- Salir\nd- Derecha\na- Izquierda" #Variable con las instrucciones del juego
-
-while True: #Bucle infinito
-  print (instrucciones) #Imprime las instrucciones
-  movimiento= input("Mover: ") #Lee hacia donde se quiere mover el muñeco
-  if movimiento == "d": #Si el movimiento es igual a d
-    juego.moverDerecha() #Llama al metodo mover derecha
-  elif movimiento == "a": #Si el movimiento es igual a a
+def jugar (self):
+  while True: #Bucle infinito
+    self.imprimirMapa()
+    print (instrucciones) #Imprime las instrucciones
+    movimiento= input("Mover: ") 
+    if movimiento == "d": #Si el movimiento es igual a d
+      juego.moverDerecha() #Llama al metodo mover derecha
+    elif movimiento == "a": #Si el movimiento es igual a a
       juego.moverIzquierda() #Llama al metodo mover izquierda
-  elif movimiento == "q": #Si el movimiento es igual a q
-    print("Saliste del juego")
+    elif movimiento == "q": #Si el movimiento es igual a q
+      print("Saliste del juego")
     break
+
+juego= Sokoban() #Crea un objeto del juego Sokoban
+juego.jugar() #Imprime el mapa
